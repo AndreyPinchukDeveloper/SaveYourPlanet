4e5f362a97d4743574f065ca98005eaeeecfe9cb
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2022-04-03 15:53:42 +0700

    Development Update #1
    - main functions and classes.

---
+++ GameObjectPlace.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;z
+
+namespace SpaceGame
+{
+    class GameObjectPlace
+    {
+        public int XCoordinate{ get; set;}
+
+        public int YCoordinate{get; set;}
+
+        
+    }
+}
---
+++ Scene.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;z
+
+namespace SpaceGame
+{
+    class Scene
+    {
+        List <GameObject> _armyOfEnemies;
+        List <GameObject> _ground;
+        List <GameObject> _playerProgectile;
+        
+        GameObject _playerShip;
+
+        #region Singleton
+        private static Scene _scene;
+        private Scene()
+        {
+
+        }
+
+        public static Scene GetScene()
+        {
+            if(_scene == null)
+            {
+                
+            }
+            return _scene;
+        }
+        #rndregion
+    }
+}
